6797a46387e57a41f8b804fb4e243fccaedf37fa
guochao <463561248@qq.com>
2021-06-25 00:24:58 +0800

    9 完成

--- lesson-9/src/kreact/react-dom.js
+++ lesson-9/src/kreact/react-dom.js
@@ -1,3 +1,5 @@
+import { Placement, Update } from "./const";
+
 // 下一个要执行的任务
 let nextUnitOfWork = null; // fiber
 let wipRoot = null; //work in progress root: fiber
@@ -36,30 +38,64 @@ function isNumberOrString(str) {
   return typeof str === "number" || typeof str === "string";
 }
 
-function updateNode(node, nextVal) {
-  const keys = Object.keys(nextVal);
-  // .filter((key) => key !== "children");
-  keys.forEach((key) => {
-    if (key === "children") {
-      if (isNumberOrString(nextVal[key])) {
-        node.textContent = nextVal[key];
+// function updateNode(node, nextVal) {
+//   const keys = Object.keys(nextVal);
+//   // .filter((key) => key !== "children");
+//   keys.forEach((key) => {
+//     if (key === "children") {
+//       if (isNumberOrString(nextVal[key])) {
+//         node.textContent = nextVal[key];
+//       }
+//     } else if (key.slice(0, 2) === "on") {
+//       // 事件
+//       const eventName = key.slice(2).toLowerCase();
+//       node.addEventListener(eventName, nextVal[key]);
+//     } else {
+//       node[key] = nextVal[key];
+//     }
+//   });
+// }
+
+function updateNode(node, prevVal, nextVal) {
+  Object.keys(prevVal)
+    // .filter(k => k !== "children")
+    .forEach((k) => {
+      if (k === "children") {
+        // 有可能是文本
+        if (isNumberOrString(prevVal[k])) {
+          node.textContent = "";
+        }
+      } else if (k.slice(0, 2) === "on") {
+        const eventName = k.slice(2).toLocaleLowerCase();
+        node.removeEventListener(eventName, prevVal[k]);
+      } else {
+        if (!(k in nextVal)) {
+          node[k] = "";
+        }
       }
-    }
-    else if(key.slice(0, 2) === 'on'){
-      // 事件
-      const eventName = key.slice(2).toLowerCase();
-      node.addEventListener(eventName, nextVal[key])
-    } 
-    else {
-      node[key] = nextVal[key];
-    }
-  });
+    });
+
+  Object.keys(nextVal)
+    // .filter(k => k !== "children")
+    .forEach((k) => {
+      if (k === "children") {
+        // 有可能是文本
+        if (isNumberOrString(nextVal[k])) {
+          node.textContent = nextVal[k] + "";
+        }
+      } else if (k.slice(0, 2) === "on") {
+        const eventName = k.slice(2).toLocaleLowerCase();
+        node.addEventListener(eventName, nextVal[k]);
+      } else {
+        node[k] = nextVal[k];
+      }
+    });
 }
 
 function createNode(workInProgress) {
   const { type, props } = workInProgress;
   let node = document.createElement(type);
-  updateNode(node, props)
+  updateNode(node, {},  props);
   return node;
 }
 
@@ -69,17 +105,52 @@ function reconcileChildren(workInProgress, children) {
   }
   const newChildren = Array.isArray(children) ? children : [children];
   let previousNewFiber = null;
+  let oldFiber = workInProgress.alternate && workInProgress.alternate.child;
   for (let i = 0; i < newChildren.length; i++) {
     let child = newChildren[i];
-    let newFiber = {
-      key: child.key, // 属性的标记节点
-      type: child.type,
-      props: { ...child.props }, //属性
-      stateNode: null,
-      child: null,
-      sibling: null,
-      return: workInProgress,
-    };
+    let same =
+      child &&
+      oldFiber &&
+      child.type === oldFiber.type &&
+      child.key === oldFiber.key;
+    let newFiber;
+    if (same) {
+      // 复用
+      newFiber = {
+        key: child.key, // 属性的标记节点
+        type: child.type,
+        props: { ...child.props }, //属性
+        stateNode: oldFiber.stateNode,
+        child: null,
+        sibling: null,
+        return: workInProgress,
+        alternate: oldFiber, //
+        flag: Update,
+      };
+    }
+    if (!same && child) {
+      // 新增
+      newFiber = {
+        key: child.key, // 属性的标记节点
+        type: child.type,
+        props: { ...child.props }, //属性
+        stateNode: null,
+        child: null,
+        sibling: null,
+        return: workInProgress,
+        flag: Placement,
+      };
+    }
+
+    if (!same && !child) {
+      // 删除
+    }
+
+    // 链表 继续
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling;
+    }
+
     if (i === 0) {
       workInProgress.child = newFiber;
     } else {
@@ -108,9 +179,9 @@ function updateFunctionComponent(workInProgress) {
   wipFiber = workInProgress;
   wipFiber.hooks = [];
   wipFiber.hookIndex = 0;
-  const {type, props} = workInProgress;
+  const { type, props } = workInProgress;
   const child = type(props);
-  reconcileChildren(workInProgress, child) 
+  reconcileChildren(workInProgress, child);
 }
 // function updateClassComponent(vnode) {
 //   const {type, props} = vnode;
@@ -131,9 +202,9 @@ function performUnitOfWork(workInProgress) {
   if (typeof type === "string") {
     // 更新原生节点
     updateHostComponent(workInProgress);
-  } else if(typeof type === "function"){
+  } else if (typeof type === "function") {
     // 函数组件
-    updateFunctionComponent(workInProgress)
+    updateFunctionComponent(workInProgress);
   }
   // 返回 下一个任务
   if (workInProgress.child) {
@@ -151,10 +222,10 @@ function performUnitOfWork(workInProgress) {
 
 // 提交 添加到 dom上
 function commitRoot() {
+  console.log("wipRoot.child", wipRoot.child);
   commitWorker(wipRoot.child);
   currentRoot = wipRoot;
   wipRoot = null;
-
 }
 
 function commitWorker(workInProgress) {
@@ -168,8 +239,21 @@ function commitWorker(workInProgress) {
     parentNodeFiber = parentNodeFiber.return;
   }
   let parentNode = parentNodeFiber.stateNode;
-  if (workInProgress.stateNode) {
+  // if (workInProgress.stateNode) {
+  //   console.log('??????', workInProgress.stateNode)
+  //   parentNode.appendChild(workInProgress.stateNode);
+  // }
+
+  // 插入
+  if (workInProgress.flag & Placement && workInProgress.stateNode) {
     parentNode.appendChild(workInProgress.stateNode);
+  } else if (workInProgress.flag & Update && workInProgress.stateNode) {
+    // 更新属性
+    updateNode(
+      workInProgress.stateNode,
+      workInProgress.alternate.props,
+      workInProgress.props
+    );
   }
 
   commitWorker(workInProgress.child);
@@ -179,14 +263,15 @@ function commitWorker(workInProgress) {
 // 回调函数
 function workLoop(IdleDeadline) {
   // debugger;
-  console.log("workLoop---");
+  // console.log("workLoop---");
   while (nextUnitOfWork && IdleDeadline.timeRemaining() > 1) {
     //执行任务链
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
   }
-  console.log('wipRoot', wipRoot)
+  // console.log('wipRoot', wipRoot)
   // commit
   if (!nextUnitOfWork && wipRoot) {
+    console.log("???");
     commitRoot();
   }
   window.requestIdleCallback(workLoop);
@@ -196,32 +281,41 @@ window.requestIdleCallback(workLoop);
 
 export function useState(init) {
   // 区分初次渲染还是 更新
-  const oldHook = wipFiber.alternate && wipFiber.alternate.hooks[wipFiber.hookIndex]
-  if(oldHook){
-
+  const oldHook =
+    wipFiber.alternate && wipFiber.alternate.hooks[wipFiber.hookIndex];
+  if (oldHook) {
   }
-  const hooks = oldHook ? 
-  {
-    state: oldHook.state,
-    queue: oldHook.queue
-  }
-  :
-  {
-    state: init,
-    queue: [],
-  }
+  const hook = oldHook
+    ? {
+        state: oldHook.state,
+        queue: oldHook.queue,
+      }
+    : {
+        state: init,
+        queue: [],
+      };
 
   const setState = (action) => {
-    hooks.queue.push(action);
-    // 假的 
+    hook.queue.push(action);
+    // 假的
     wipRoot = {
+      type: currentRoot.type,
       stateNode: currentRoot.stateNode,
       props: currentRoot.props,
-      alternate: currentRoot
-    }
-    nextUnitOfWork = wipFiber
-  }
-  return [init, setState]
+      // child: currentRoot.child,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+  };
+
+  // 实现批量更新
+
+  hook.queue.forEach((action) => {
+    hook.state = action;
+  });
+  wipFiber.hooks.push(hook);
+  wipFiber.hookIndex++;
+  return [hook.state, setState];
 }
 
 export default {
