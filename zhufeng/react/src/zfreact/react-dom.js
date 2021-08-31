b9272d09d8b4ef73b0f2bfacb2ba51e5824669ff
guochao <463561248@qq.com>
2021-08-31 22:17:21 +0800

    添加珠峰代码

---
+++ zhufeng/react/src/zfreact/react-dom.js
@@ -0,0 +1,60 @@
+function render(vnode, container) {
+  // 1. 虚拟dom 转换成 真实 dom
+  const dom = createDOM(vnode, container);
+  // 2. dom 添加到 容器中
+  container.appendChild(dom);
+}
+export function mount(vdom, container) {
+  let newDOM = createDOM(vdom);
+  container.appendChild(newDOM);
+}
+
+function createDOM(vnode, container) {
+  console.log("vnode", vnode);
+  const { type, props } = vnode;
+  let dom;
+  // 原生组件
+  dom = document.createElement(type);
+  if (props) {
+    // 使用虚拟DOM的属性更新刚创建出来的真实DOM的属性
+    updateProps(dom, {}, props);
+    //如果只有一个儿子，并且这个儿子是一个文本
+    if (typeof props.children === "object" && props.children.type) {
+      //把儿子变成真实DOM插到自己身上
+      console.log("文本");
+      mount(props.children, dom);
+      //如果儿子是一个数的话，说明儿子不止一个
+    } else if (Array.isArray(props.children)) {
+      console.log("文本1");
+      // reconcileChildren(props.children,dom);
+    }
+  }
+  vnode.dom = dom;
+  return dom;
+}
+/**
+ *
+ * @param {*} dom
+ * @param {*} oldProps
+ * @param {*} newProps
+ */
+function updateProps(dom, oldProps, newProps) {
+  for (let key in newProps) {
+    if (key === "children") continue; // 不在这里处理
+    if (key === "style") {
+      // ? 只是修改新的值吗？
+      let styleObj = newProps.style;
+      for (let attr in styleObj) {
+        dom.style[attr] = styleObj[attr];
+      }
+    } else if (key.startsWith("on")) {
+      // 添加事件 这个只是一个简单的判断
+    } else {
+      // 其他的属性
+      dom[key] = newProps[key];
+    }
+  }
+}
+export default {
+  render,
+};
---
+++ zhufeng/react/src/zfreact/react.js
@@ -0,0 +1,34 @@
+import {wrapToVdom} from './utils';
+/**
+ * @param {*} type 元素的类型
+ * @param {*} config 配置对象
+ * @param {*} children  儿子或儿子们
+ */
+function createElement(type,config,children){
+    let ref;
+    let key;
+    if(config){
+        delete config.__source;
+        delete config.__self;
+        ref = config.ref;
+        delete config.ref;
+        key = config.key;
+        delete config.key;
+    }
+   let props = {...config};
+   if(arguments.length>3){
+       props.children=Array.prototype.slice.call(arguments,2).map(wrapToVdom);
+   }else{
+        props.children=wrapToVdom(children);
+   }
+   return {
+       type,
+       ref,
+       key,
+       props
+   }
+}
+const React = {
+  createElement,
+}
+export default React;
